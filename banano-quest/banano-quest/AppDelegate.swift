fd689a8e383c3b4a74e246f51d181a0eee2ec7a5
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2018-07-02 09:16:20 -0400

    disabled initial quest list load

--- banano-quest/banano-quest/AppDelegate.swift
+++ banano-quest/banano-quest/AppDelegate.swift
@@ -20,7 +20,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Override point for customization after application launch.
         
         // Initial quest list download
-        Networking.getQuestList()
+        // Networking.getQuestList()
         
         return true
     }
