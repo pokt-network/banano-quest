95eafa4956d17f1ac3cb8e3061acec0ff50f5be5
Michael O'Rourke <>
2018-07-03 05:59:32 -0400

    completed UI for quest creation

---
+++ banano-quest/banano-quest/View Controllers/CreateQuestViewController.swift
@@ -0,0 +1,63 @@
+//
+//  CreateQuestViewController.swift
+//  banano-quest
+//
+//  Created by Michael O'Rourke on 7/2/18.
+//  Copyright © 2018 Michael O'Rourke. All rights reserved.
+//
+
+import UIKit
+
+class CreateQuestViewController: UIViewController {
+
+    @IBOutlet weak var addColorButton: UIButton!
+    @IBOutlet weak var addColorView: UIView!
+    @IBOutlet weak var questNameTextField: UITextField!
+    @IBOutlet weak var prizeAmountUSDTextField: UITextField!
+    @IBOutlet weak var prizeAmountETHTextField: UITextField!
+    @IBOutlet weak var howManyBananosTextField: UITextField!
+    @IBOutlet weak var hintTextView: UITextView!
+    @IBOutlet weak var isTherePrizeSwitch: UISwitch!
+    @IBOutlet weak var hintTextCountLabel: UILabel!
+    @IBOutlet weak var currentUSDBalanceLabel: UILabel!
+    @IBOutlet weak var currentETHBalanceLabel: UILabel!
+    @IBOutlet weak var gasCostUSDLabel: UILabel!
+    @IBOutlet weak var gasCostETHLabel: UILabel!
+    
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        addColorView.layer.cornerRadius = addColorView.frame.size.width / 2
+        addColorView.clipsToBounds = true
+        
+        addColorButton.layer.cornerRadius = addColorButton.frame.size.width / 2
+        addColorButton.clipsToBounds = true
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    @IBAction func addColorPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func addLocationPressed(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
