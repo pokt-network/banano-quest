e684f7552b3f5ea015ffc92448868af4e2618484
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2018-07-16 19:17:25 -0400

    Moved dismissKeyboard to UIViewController extension, CreateQuestVC in progress, updated createQuest UI

--- banano-quest/banano-quest/Extensions/UIViewController+extension.swift
+++ banano-quest/banano-quest/Extensions/UIViewController+extension.swift
@@ -40,4 +40,8 @@ extension UIViewController {
         
         return alert
     }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
 }
--- banano-quest/banano-quest/ViewControllers/CreateQuestViewController.swift
+++ banano-quest/banano-quest/ViewControllers/CreateQuestViewController.swift
@@ -24,9 +24,6 @@ class CreateQuestViewController: UIViewController {
     @IBOutlet weak var gasCostUSDLabel: UILabel!
     @IBOutlet weak var gasCostETHLabel: UILabel!
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -34,33 +31,51 @@ class CreateQuestViewController: UIViewController {
     }
 
     override func viewWillAppear(_ animated: Bool) {
+        // UI Settings
         addColorView.layer.cornerRadius = addColorView.frame.size.width / 2
         addColorView.clipsToBounds = true
         
         addColorButton.layer.cornerRadius = addColorButton.frame.size.width / 2
         addColorButton.clipsToBounds = true
-    }
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+        hintTextView.layer.borderWidth = 2.0
+        hintTextView.layer.cornerRadius = 5
+        hintTextView.layer.borderColor = UIColor(red: (253/255), green: (204/255), blue: (48/255), alpha: 1.0).cgColor
+        
+        // Prize switch toggle action
+        isTherePrizeSwitch.addTarget(self, action: #selector(switchChanged), for: UIControlEvents.valueChanged)
+
+        // Prize is enabled by default
+        isPrizeEnabled(bool: true)
+        
+        // Gesture recognizer that dismiss the keyboard when tapped outside
+        let tapOutside: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(self.dismissKeyboard))
+        tapOutside.cancelsTouchesInView = false
+        
+        view.addGestureRecognizer(tapOutside)
     }
     
-
+    @objc func switchChanged(switchButton: UISwitch) {
+        // Checks if the button is On or Off to disable/enable prize textFields
+        if switchButton.isOn {
+            isPrizeEnabled(bool: true)
+        }else {
+            isPrizeEnabled(bool: false)
+        }
+    }
+    
+    func isPrizeEnabled(bool: Bool) {
+        // Enables/Disables prize text fields
+        prizeAmountETHTextField.isEnabled = bool
+        prizeAmountUSDTextField.isEnabled = bool
+    }
+    
     @IBAction func addColorPressed(_ sender: Any) {
         
     }
     
     @IBAction func addLocationPressed(_ sender: Any) {
+        
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
