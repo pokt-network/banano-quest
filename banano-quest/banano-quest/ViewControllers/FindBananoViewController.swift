146d001746276318d711d35e5a960463de761f7c
Pabel Nunez Landestoy <pabelnunezl@gmail.com>
2018-08-28 09:31:03 -0400

    added balance check

--- banano-quest/banano-quest/ViewControllers/FindBananoViewController.swift
+++ banano-quest/banano-quest/ViewControllers/FindBananoViewController.swift
@@ -16,15 +16,24 @@ import Pocket
 class FindBananoViewController: ARViewController, ARDataSource, AnnotationViewDelegate {
     @IBOutlet weak var claimButton: UIButton!
     @IBOutlet weak var permissionsView: UIView!
-    
+
     fileprivate var arViewController: ARViewController!
     var bananoLocation: CLLocation?
     var currentUserLocation: CLLocation?
     var currentQuest: Quest?
+    var currentPlayer: Player?
     var questProof: QuestProofSubmission?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
+        // Current Player
+        do {
+            currentPlayer = try Player.getPlayer(context: CoreDataUtils.mainPersistentContext)
+        } catch {
+            self.present(self.bananoAlertView(title: "Error", message: "An error ocurred retrieving your account information, please try again"), animated: true)
+            return
+        }
 
         // AR Setup
         self.dataSource = self
@@ -41,44 +50,43 @@ class FindBananoViewController: ARViewController, ARDataSource, AnnotationViewDe
             print("Failed to refresh view with error: \(error)")
         }
     }
-    
+
     override func refreshView() throws {
         // Check for camera permission
         checkCameraAccess()
     }
-    
+
     func setupBananoAR() {
         // Banano Location is generated based in the user location after is confirmed the user is withing the quest
         // completion range.
         let coordinates = getBananoLocation()
         bananoLocation = CLLocation(latitude: coordinates.latitude, longitude: coordinates.longitude)
-        
+
         let questLocation = CLLocation(latitude: bananoLocation?.coordinate.latitude ?? 0.0, longitude: bananoLocation?.coordinate.longitude ?? 0.0)
         let bananoLocationC = CLLocation(coordinate: questLocation.coordinate, altitude: CLLocationDistance.init(currentUserLocation?.altitude ?? 0), horizontalAccuracy: CLLocationAccuracy.init(0), verticalAccuracy: CLLocationAccuracy.init(0), timestamp: Date.init())
-        
+
         let distance = bananoLocationC.distance(from: currentUserLocation!)
-        
+
         if distance <= 50 {
             let annotation = ARAnnotation()
             annotation.title = currentQuest?.name
             annotation.location = bananoLocationC
-            
+
             // AR options
             // Max distance between the player and the Banano
             maxDistance = 50
-            
+
             // We add the annotations that for Banano quest is 1 at a time
             setAnnotations([annotation])
-            
         }else {
             let alertController = bananoAlertView(title: "Not in range", message: "\(currentQuest?.name ?? "") banano is not within 50 meters of your current location")
             present(alertController, animated: false, completion: nil)
         }
     }
-    
+
     func checkCameraAccess() {
         let cameraIsAvailable = UIImagePickerController.isSourceTypeAvailable(.camera)
-        
+
         if cameraIsAvailable {
             let status = AVCaptureDevice.authorizationStatus(for: AVMediaType.video)
             switch status {
@@ -87,17 +95,17 @@ class FindBananoViewController: ARViewController, ARDataSource, AnnotationViewDe
                 setupBananoAR()
             case .denied:
                 permissionsView.isHidden = false
-                
+
                 let alertView = UIAlertController(title: "Camera Access", message: "Banano Quest is requesting camera access, will you like to enable access to the camera?", preferredStyle: .alert)
-                
+
                 alertView.addAction(UIAlertAction(title: "Ok", style: .default, handler: { (UIAlertAction) in
                     self.openAppSettings()
                 }))
-                
+
                 alertView.addAction(UIAlertAction(title: "No", style: .cancel, handler: nil))
-                
+
                 present(alertView, animated: false, completion: nil)
-                
+
             case .notDetermined:
                 requestCameraAccess()
             default:
@@ -105,27 +113,27 @@ class FindBananoViewController: ARViewController, ARDataSource, AnnotationViewDe
             }
         } else {
             let alertView = bananoAlertView(title: "Error", message: "Device has not cameras available")
-            
+
             present(alertView, animated: false, completion: nil)
         }
     }
-    
+
     func openAppSettings() {
         guard let settingsUrl = URL(string: UIApplicationOpenSettingsURLString) else {
             return
         }
-        
+
         if UIApplication.shared.canOpenURL(settingsUrl) {
             UIApplication.shared.open(settingsUrl, completionHandler: { (success) in
                 print("Settings opened: \(success)")
             })
         }
-        
+
     }
-    
+
     func requestCameraAccess() {
         let alertView = UIAlertController( title: "Camera Access", message: "Banano Quest would like to access your Camera to continue.", preferredStyle: .alert )
-        
+
         let allowAction = UIAlertAction(title: "Allow", style: .default, handler: { (alert) -> Void in
             if let _ = AVCaptureDevice.default(.builtInWideAngleCamera, for: AVMediaType.video, position: .back) {
                 AVCaptureDevice.requestAccess(for: AVMediaType.video) { (granted) in
@@ -139,7 +147,7 @@ class FindBananoViewController: ARViewController, ARDataSource, AnnotationViewDe
                 print("Back Camera not available")
             }
         })
-        
+
         alertView.addAction(allowAction)
         let declineAction = UIAlertAction(title: "Not Now", style: .cancel) { (alert) in
             print("Permission denied")
@@ -147,7 +155,7 @@ class FindBananoViewController: ARViewController, ARDataSource, AnnotationViewDe
         alertView.addAction(declineAction)
         present(alertView, animated: true, completion: nil)
     }
-    
+
     func locationWithBearing(bearing:Double, distanceMeters:Double, origin:CLLocationCoordinate2D) -> CLLocationCoordinate2D {
         let distRadians = distanceMeters / (6372797.6) // earth radius in meters
 
@@ -254,18 +262,30 @@ class FindBananoViewController: ARViewController, ARDataSource, AnnotationViewDe
     @IBAction func grantCameraAccess(_ sender: Any) {
         requestCameraAccess()
     }
-    
+
     @IBAction func backButtonPressed(_ sender: Any) {
         self.dismiss(animated: false, completion: nil)
     }
-    
+
     @IBAction func claimButtonPressed(_ sender: Any) {
         self.retrieveGasEstimate { (gasEstimateWei) in
             if let gasEstimate = gasEstimateWei {
+                // Transaction estimate
                 let gasEstimateEth = EthUtils.convertWeiToEth(wei: gasEstimate)
                 let gasEstimateUSD = EthUtils.convertEthAmountToUSD(ethAmount: gasEstimateEth)
+                // Player balance
+                let playerEthBalance = EthUtils.convertWeiToEth(wei: BigInt(self.currentPlayer?.balanceWei ?? "0")!)
+                let playerUSDBalance = EthUtils.convertEthAmountToUSD(ethAmount: playerEthBalance)
+
+                if gasEstimateUSD > playerUSDBalance {
+                    let message = String.init(format: "Insufficient funds, Total transaction cost: %@ USD - %@ ETH. Current player balance: %@ USD - %@ ETH", String.init(format: "%.4f", gasEstimateUSD), String.init(format: "%.4f", gasEstimateEth), String.init(format: "%.4f", playerUSDBalance), String.init(format: "%.4f", playerEthBalance))
+                    self.noBalanceHandler(message: message)
+
+                    return
+                }
+
                 let message = String.init(format: "Total transaction cost: %@ USD - %@ ETH. Press OK to claim your Banano", String.init(format: "%.4f", gasEstimateUSD), String.init(format: "%.4f", gasEstimateEth))
-                
+
                 let txDetailsAlertView = self.bananoAlertView(title: "Transaction Details", message: message) { (uiAlertAction) in
                     do {
                         let player = try Player.getPlayer(context: CoreDataUtils.mainPersistentContext)
@@ -288,7 +308,21 @@ class FindBananoViewController: ARViewController, ARDataSource, AnnotationViewDe
             }
         }
     }
-    
+
+    func noBalanceHandler(message: String) {
+        let alertView = bananoAlertView(title: "Failed", message: message)
+        let addBalance = UIAlertAction.init(title: "Add Balance", style: .default) { (UIAlertAction) in
+            do {
+                let vc = try self.instantiateViewController(identifier: "addBalanceViewControllerID", storyboardName: "Profile")
+                self.present(vc, animated: false, completion: nil)
+            }catch let error as NSError {
+                print("Failed to instantiate addBalanceViewControllerID with error: \(error)")
+            }
+        }
+        alertView.addAction(addBalance)
+        present(alertView, animated: false, completion: nil)
+    }
+
     func retrieveGasEstimate(handler: @escaping (BigInt?) -> Void) {
         do {
             let player = try Player.getPlayer(context: CoreDataUtils.mainPersistentContext)
